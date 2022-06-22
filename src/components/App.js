98f8aac10a67da5803eeaea70b8a568c6077ecd8
Divyanshu Tripathi <104244101+Divyanshu-2022@users.noreply.github.com>
2022-06-22 22:52:36 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,10 @@ import '../styles/App.css';
 const App = () => {
   return (
     <div id="main"></div>
+    <h1 data-ns-test="project-namr"> Online food selling App</h1>
+    <dl data-ns-test="project-description">
+    <dd>Sellling Sea Food , fishes nad meats  online</dd>
+    <dl>
   )
 }
 
